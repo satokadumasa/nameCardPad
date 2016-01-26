c8dfb2bc21458aee44552f8a471af4a4bb1b141f
佐藤憲匡 <satoukadumasa@satoukentadashi-no-macbook-air.local>
2016-01-26 15:28:49 +0900

    Initial Commit

---
+++ nameCardPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nameCardPad
+//
+//  Created by 佐藤憲匡 on 2016/01/26.
+//  Copyright © 2016年 佐藤憲匡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
