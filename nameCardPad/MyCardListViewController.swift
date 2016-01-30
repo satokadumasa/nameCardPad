7213ca65116f0c27432e4ad4d2221ab79eb00f0c
git <sato.kadumasa@gmail.com>
2016-01-30 19:27:58 +0900

    ローカルデータベース定義を追加

--- nameCardPad/MyCardListViewController.swift
+++ nameCardPad/MyCardListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import CoreData
 
 //CardListViewController
-class MyCardListViewController: UIViewController {
+class MyCardListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // ナビバーの右上ボタンを用意
     var addBtn: UIBarButtonItem!
@@ -30,9 +30,24 @@ class MyCardListViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = addBtn
     }
     
+    
     // viewDidLoadは最初の一回しか呼ばれないので、viewWillAppearを使うよ
     override func viewWillAppear(animated: Bool) {
-        
+//        // CoreDataからデータを読み込んで配列memosに格納する
+//        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+//        let toDoContext: NSManagedObjectContext = appDel.managedObjectContext
+//        let toDoRequest: NSFetchRequest = NSFetchRequest(entityName: "ToDoStore")
+//        // 並び順をdateの、昇順としてみる
+//        toDoRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+//        toDoRequest.returnsObjectsAsFaults = false
+//        var results = toDoContext.executeFetchRequest(toDoRequest, error: nil) as [ToDoStore]!
+//        memos = []
+//        for data in results {
+//            memos.append(data.memo)
+//        }
+//        
+//        // テーブル情報を更新する
+//        self.piyo.reloadData()
         
     }
     
@@ -45,6 +60,15 @@ class MyCardListViewController: UIViewController {
         let cardaddView = MyCardAddViewController()
         self.navigationController?.pushViewController(cardaddView, animated: true)
     }
-
     
-}
+    // セルの行数を指定
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memos.count
+    }
+    
+    // セルの値を設定
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("data", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel!.text = memos[indexPath.row] as String
+        return cell
+    }}
